d52b03c87993c061efd4254d2c1a54478cdded63
Apurva Anand <apurva.anand1203@gmail.com>
2024-02-04 01:32:31 +0530

    Updated contact page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -8,8 +9,23 @@ import { Title } from '@angular/platform-browser';
 })
 export class HomeComponent {
   // using angular pre-built services Title to set our title.
-  constructor(private titleService:Title) {
+  constructor(private titleService:Title, private http: HttpClient) {
     this.titleService.setTitle('Apurva-Home')
+  }
+
+  downloadFile(): void {
+    const fileUrl = "../../assets/Apurva_Resume.pdf"; // Replace with the actual path to your file
+
+    this.http.get(fileUrl, { responseType: 'arraybuffer' })
+      .subscribe((data: ArrayBuffer) => {
+        const blob = new Blob([data], { type: 'application/pdf' });
+
+        // Trigger file download
+        const link = document.createElement('a'); // It creates anchor tag
+        link.href = window.URL.createObjectURL(blob);
+        link.download = 'Resume.pdf'; // Replace with the desired filename
+        link.click();
+      });
   }
 
 }
--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
@@ -8,23 +7,8 @@ import { Title } from '@angular/platform-browser';
   styleUrl: './resume.component.css'
 })
 export class ResumeComponent {
-  constructor(private titleService:Title, private http: HttpClient) {
+  constructor(private titleService:Title) {
     this.titleService.setTitle('Apurva - Resume')
-  }
-
-  downloadFile(): void {
-    const fileUrl = "../../assets/Apurva_Resume.pdf"; // Replace with the actual path to your file
-
-    this.http.get(fileUrl, { responseType: 'arraybuffer' })
-      .subscribe((data: ArrayBuffer) => {
-        const blob = new Blob([data], { type: 'application/pdf' });
-
-        // Trigger file download
-        const link = document.createElement('a'); // It creates anchor tag
-        link.href = window.URL.createObjectURL(blob);
-        link.download = 'Resume.pdf'; // Replace with the desired filename
-        link.click();
-      });
   }
 
 }
